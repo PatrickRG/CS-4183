f6aeae0301a74485c43ecc0c80a9560632bd24c3
PatrickRG <patrick.goddard@okstate.edu>
2016-12-01 00:19:45 -0600

    Add files via upload

--- Sprint-3/goddardCar.js
+++ Sprint-3/goddardCar.js
@@ -109,3 +109,22 @@ function driveScript(sceneNode)
         pressedKeys[39] = false;
     }
 }
+
+function rotateScript(sceneNode)
+{
+    var rotationSpeed = sceneNode.userData["rotationSpeed"];
+    sceneNode.rotateOnAxis(YAXIS, rotationSpeed*frameDuration);
+    if (pressedKeys[UP_ARROW]){
+            debug("wehere");
+			cycleCamera();
+	}
+}
+
+function getChildByName(sceneNode, childName)
+{
+	var children = sceneNode.children;
+	for (var i=0; i<children.length; i++) {
+		if (children[i].name == childName)
+            return children[i];
+	}
+}
